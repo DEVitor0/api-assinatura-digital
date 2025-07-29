bed374e6ffcc1a5e53ea3bb6567cd2818dfecce7
DEVitor0 <devitormoreira@gmail.com>
2025-07-29 15:41:55 -0300

    docs(validator-servc): rotas e APIs documentadas na rota /api/docs com swagger

--- validator-service/src/app.ts
+++ validator-service/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import validatorRoutes from './routes/validator.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/validator', validatorRoutes);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
